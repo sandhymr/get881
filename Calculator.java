8c9195a92c881afd7300ee334ea99e1f3f8bf492
sandhymr <35697568+sandhymr@users.noreply.github.com>
2020-12-17 13:39:42 +0530

    first commit
    Created Calculator class and added add method.

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+class Calculator{
+   public static int add(int num1,int num2){
+        return num1+num2;
+   }
+    
+   public static void main(String[] args){
+     System.out.println(add(20,90));
+   }
+}
